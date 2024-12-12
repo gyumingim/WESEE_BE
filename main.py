c7f20918ddb5499da4943321416ef8ec4a2e1165
gyumingim <a31620504@gmail.com>
2024-12-12 15:28:59 +0900

    글자를 점자로 변환하는 기능 추가

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import io
 import base64
 import gtts
 import playsound as ps
+import hbcvt
 
 # 인스턴스 설정
 app = FastAPI()
@@ -29,6 +30,20 @@ app.add_middleware(
     allow_methods=["*"],  # 모든 HTTP 메서드 허용
     allow_headers=["*"],  # 모든 HTTP 헤더 허용
 )
+
+def decimal_to_braille_binary(dot_letters):
+    braille_binaries = []
+    
+    for decimal_dot in dot_letters:
+        # 십진수를 6자리 2진수로 변환
+        binary_dot = format(decimal_dot, '06b')
+        
+        # 문자열 2진수를 정수 리스트로 변환
+        binary_list = [int(bit) for bit in binary_dot]
+        
+        braille_binaries.append(binary_list[:6])
+    
+    return braille_binaries
 
 # base64를 이미지로 decode해서 png로 저장
 def save_image_to_base64(base64_image):
@@ -52,12 +67,29 @@ def play_TTS(result):
 
 # base64 문자열 입력받고
 # 결과로 OCR 인식 글자 return
-@app.post("/api/ocr")
+@app.post("/ocr")
 def api_ocr(request: OCRRequest):
     save_image_to_base64(request.image)
     result = ocr_base64()
 
-    play_TTS(result)
+    # play_TTS(result)
+
+    dot_letters = []
+    for letter in hbcvt.h2b.text(result):
+        for consonant in letter[1]:
+            dot_letter = 0
+            ord_value = 64
+            print("------원본점자-----")
+            print(consonant[1][0])
+            for dot in consonant[1][0]:
+                dot_letter += int(dot) * ord_value
+                ord_value /= 2
+            dot_letters.append(int(dot_letter))
+    print("------10진수 점자-----")
+    print(dot_letters)
+    converted_braille = decimal_to_braille_binary(dot_letters)
+    print("------복호화 점자-----")
+    print(converted_braille)
 
     return {"result": result}
 
---
+++ test2.py
@@ -0,0 +1,4 @@
+import hbcvt
+
+
+print(hbcvt.h2b.text("꽃이 피었어!"))
